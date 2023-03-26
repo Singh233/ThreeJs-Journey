42a36e5b062ccec49db75a27a9a3ee411d57a296
Sanambir Singh <sanambir123@gmail.com>
2023-03-26 22:26:26 +0530

    Complete position, scale, rotation properties

--- 05-transform-objects/src/script.js
+++ 05-transform-objects/src/script.js
@@ -9,10 +9,40 @@ const scene = new THREE.Scene()
 /**
  * Objects
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+const group = new THREE.Group();
+scene.add(group);
+
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
+
+group.add(cube1);
+
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+);
+
+cube2.position.x = -2;
+
+group.add(cube2);
+
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0x0000ff })
+);
+
+cube3.position.x = 2;
+group.add(cube3);
+
+// Positioning
+group.position.y = 1;
+
+// Axes helper (for debugging)
+const axesHelper = new THREE.AxesHelper(2);
+scene.add(axesHelper);
+
 
 /**
  * Sizes
@@ -26,8 +56,9 @@ const sizes = {
  * Camera
  */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.z = 3
+camera.position.set(0, 0, 3)
 scene.add(camera)
+
 
 /**
  * Renderer
@@ -37,3 +68,4 @@ const renderer = new THREE.WebGLRenderer({
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.render(scene, camera)
+
