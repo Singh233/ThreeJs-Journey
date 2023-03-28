1e5735140a48e0738c1be999c8f5bca2851b52e2
Sanambir Singh <sanambir123@gmail.com>
2023-03-28 15:39:21 +0530

    Add first texture to cube manually

--- 11-textures/src/script.js
+++ 11-textures/src/script.js
@@ -1,5 +1,19 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import imageSource from '/textures/door/color.jpg';
+
+/**
+ * Textures
+ */
+
+const image = new Image();
+const texture = new THREE.Texture(image);
+
+image.onload = () => {
+    texture.needsUpdate = true;
+}
+
+image.src = '/textures/door/color.jpg'
 
 /**
  * Base
@@ -14,7 +28,7 @@ const scene = new THREE.Scene()
  * Object
  */
 const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const material = new THREE.MeshBasicMaterial({ map: texture })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
