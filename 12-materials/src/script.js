41c3cf0a75d93c8bdf1d6d027ad116a5865ba67f
Sanambir Singh <Sanambir123@gmail.com>
2023-03-29 11:59:55 +0530

    Prepare scene with sphere, plane and torus geometry

--- 12-materials/src/script.js
+++ 12-materials/src/script.js
@@ -1,5 +1,25 @@
 import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
+
+/**
+ * Textures
+ */
+
+const textureLoader = new THREE.TextureLoader();
+
+const doorColorTexture = textureLoader.load('/textures/door/color.jpg');
+const doorAlphaTexture = textureLoader.load('/textures/door/alpha.jpg');
+const doorAmbientOcclusionTexture = textureLoader.load('/textures/door/ambientOcclusion.jpg');
+const doorHeightTexture = textureLoader.load('/textures/door/height.jpg');
+const doorNormalTexture = textureLoader.load('/textures/door/normal.jpg');
+const doorMetalnessTexture = textureLoader.load('/textures/door/metalness.jpg');
+const doorRoughnessTexture = textureLoader.load('/textures/door/roughness.jpg');
+
+const matcapTexture = textureLoader.load('/textures/matcaps/1.png');
+const gradientsTexture = textureLoader.load('/textures/gradients/3.jpg');
+
+
 
 /**
  * Base
@@ -9,6 +29,34 @@ const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
+
+/**
+ * Objects
+ */
+
+const material = new THREE.MeshBasicMaterial();
+// material.map = doorColorTexture
+
+const sphere = new THREE.Mesh(
+    new THREE.SphereGeometry(0.5, 16, 16),
+    material
+)
+sphere.position.x = -1.5
+
+
+const plane = new THREE.Mesh(
+    new THREE.PlaneGeometry(1, 1),
+    material
+)
+
+const torus = new THREE.Mesh(
+    new THREE.TorusGeometry(0.3, 0.2, 16, 32),
+    material
+)
+
+torus.position.x = 1.5;
+
+scene.add(sphere, plane, torus);
 
 /**
  * Sizes
@@ -65,8 +113,19 @@ const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
 
+    // Update objects
+    sphere.rotation.y = elapsedTime * 0.1;
+    plane.rotation.y = elapsedTime * 0.1;
+    torus.rotation.y = elapsedTime * 0.1;
+
+    sphere.rotation.x = elapsedTime * 0.15;
+    plane.rotation.x = elapsedTime * 0.15;
+    torus.rotation.x = elapsedTime * 0.15;
+
     // Update controls
     controls.update()
+
+    
 
     // Render
     renderer.render(scene, camera)
