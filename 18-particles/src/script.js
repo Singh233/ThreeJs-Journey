db5465b981916858ad054cf24dd548265f5ebce5
Sanambir Singh <Sanambir123@gmail.com>
2023-04-06 19:43:09 +0530

    Add first particles

--- 18-particles/src/script.js
+++ 18-particles/src/script.js
@@ -20,13 +20,20 @@ const scene = new THREE.Scene()
 const textureLoader = new THREE.TextureLoader()
 
 /**
- * Test cube
+ * Particles
  */
-const cube = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial()
-)
-scene.add(cube)
+// Geometry
+const particlesGeometry = new THREE.SphereGeometry(1, 32, 32);
+
+//Material
+const particlesMaterial = new THREE.PointsMaterial({
+    size: 0.02,
+    sizeAttenuation: true
+})
+
+//Points
+const particles = new THREE.Points(particlesGeometry, particlesMaterial);
+scene.add(particles);
 
 /**
  * Sizes
