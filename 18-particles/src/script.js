de91bfd4b07a25e97d1f6ce2e207d0d16963812e
Sanambir Singh <Sanambir123@gmail.com>
2023-04-07 12:18:25 +0530

    Add color to particles

--- 18-particles/src/script.js
+++ 18-particles/src/script.js
@@ -17,19 +17,50 @@ const scene = new THREE.Scene()
 /**
  * Textures
  */
-const textureLoader = new THREE.TextureLoader()
+const textureLoader = new THREE.TextureLoader();
+
+const particleTexture = textureLoader.load('/textures/particles/2.png');
 
 /**
  * Particles
  */
 // Geometry
-const particlesGeometry = new THREE.SphereGeometry(1, 32, 32);
+const particlesGeometry = new THREE.BufferGeometry();
+const count = 5000;
 
+const positions = new Float32Array(count * 3);
+const colors = new Float32Array(count * 3);
+
+
+for (let i = 0; i < count * 3; i++) {
+    positions[i] = (Math.random() - 0.5) * 10;
+    colors[i] = Math.random();
+
+}
+
+particlesGeometry.setAttribute(
+    'position',
+    new THREE.BufferAttribute(positions, 3)
+)
+particlesGeometry.setAttribute(
+    'color',
+    new THREE.BufferAttribute(colors, 3)
+)
 //Material
 const particlesMaterial = new THREE.PointsMaterial({
-    size: 0.02,
+    size: 0.1,
     sizeAttenuation: true
 })
+
+// particlesMaterial.color = new THREE.Color('#ff88cc');
+// particlesMaterial.map = particleTexture
+particlesMaterial.transparent = true;
+particlesMaterial.alphaMap = particleTexture;
+// particlesMaterial.alphaTest = 0.001;
+// particlesMaterial.depthTest = false;
+particlesMaterial.depthWrite = false;
+particlesMaterial.blending = THREE.AdditiveBlending;
+particlesMaterial.vertexColors = true;
 
 //Points
 const particles = new THREE.Points(particlesGeometry, particlesMaterial);
