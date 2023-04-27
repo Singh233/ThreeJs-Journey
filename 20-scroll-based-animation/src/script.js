2206a5ec07492c4a9c85d15a682e55b341220d46
Sanambir Singh <Sanambir123@gmail.com>
2023-04-27 22:43:25 +0530

    Add primitive objects, lights, textures

--- 20-scroll-based-animation/src/script.js
+++ 20-scroll-based-animation/src/script.js
@@ -12,6 +12,9 @@ const parameters = {
 
 gui
     .addColor(parameters, 'materialColor')
+    .onChange(() => {
+        material.color.set(parameters.materialColor)
+    })
 
 /**
  * Base
@@ -23,13 +26,54 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 /**
- * Test cube
+ * Objects
+ * 
  */
-const cube = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial({ color: '#ff0000' })
+// Texture
+const textureLoader = new THREE.TextureLoader();
+const gradientTexture = textureLoader.load('textures/gradients/3.jpg');
+gradientTexture.magFilter = THREE.NearestFilter
+// Material
+const material = new THREE.MeshToonMaterial({ 
+    color: parameters.materialColor,
+    gradientMap: gradientTexture
+})
+
+
+// Meshes
+const objectsDistance = 4;
+
+const mesh1 = new THREE.Mesh(
+    new THREE.TorusGeometry(1, 0.4, 16, 60),
+    material
 )
-scene.add(cube)
+
+const mesh2 = new THREE.Mesh(
+    new THREE.ConeGeometry(1, 2, 32),
+    material
+)
+
+const mesh3 = new THREE.Mesh(
+    new THREE.TorusKnotGeometry(0.8, 0.35, 100, 16),
+    material
+)
+mesh1.position.y = - objectsDistance * 0;
+mesh2.position.y = - objectsDistance * 1;
+mesh3.position.y = - objectsDistance * 2;
+
+scene.add(mesh1, mesh2, mesh3);
+
+const sectionMeshes = [ mesh1, mesh2, mesh3 ];
+
+
+/**
+ * Lights
+ */
+
+const directionalLight = new THREE.DirectionalLight('#ffffff', 1);
+directionalLight.position.set(1, 1, 0);
+scene.add(directionalLight);
+
 
 /**
  * Sizes
@@ -66,7 +110,8 @@ scene.add(camera)
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+    canvas: canvas,
+    alpha: true
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
@@ -79,6 +124,13 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+
+    // Animate meshes
+    for (const mesh of sectionMeshes) {
+        mesh.rotation.x = elapsedTime * 0.1;
+        mesh.rotation.y = elapsedTime * 0.12;
+
+    }
 
     // Render
     renderer.render(scene, camera)
