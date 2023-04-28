b06f87de817a1865123107ca805e7852171a0389
Sanambir Singh <Sanambir123@gmail.com>
2023-04-28 12:52:26 +0530

    Move camera on scroll and add parallax effect to objects

--- 20-scroll-based-animation/src/script.js
+++ 20-scroll-based-animation/src/script.js
@@ -61,6 +61,10 @@ mesh1.position.y = - objectsDistance * 0;
 mesh2.position.y = - objectsDistance * 1;
 mesh3.position.y = - objectsDistance * 2;
 
+mesh1.position.x = 2;
+mesh2.position.x = - 2;
+mesh3.position.x = 2;
+
 scene.add(mesh1, mesh2, mesh3);
 
 const sectionMeshes = [ mesh1, mesh2, mesh3 ];
@@ -98,13 +102,18 @@ window.addEventListener('resize', () =>
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
+
 /**
  * Camera
  */
+// Group
+const cameraGroup = new THREE.Group();
+scene.add(cameraGroup);
+
 // Base camera
 const camera = new THREE.PerspectiveCamera(35, sizes.width / sizes.height, 0.1, 100)
 camera.position.z = 6
-scene.add(camera)
+cameraGroup.add(camera)
 
 /**
  * Renderer
@@ -117,13 +126,47 @@ renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
 /**
+ * Scroll
+ */
+let scrollY = window.scrollY;
+
+window.addEventListener('scroll', () => {
+    scrollY = window.scrollY;
+
+});
+
+/**
+ * Cursor
+ */
+
+const cursor = {}
+cursor.x = 0;
+cursor.y = 0;
+
+window.addEventListener('mousemove', (e) => {
+    cursor.x = e.clientX / sizes.width - 0.5;
+    cursor.y = e.clientY / sizes.height - 0.5;
+})
+
+/**
  * Animate
  */
-const clock = new THREE.Clock()
+const clock = new THREE.Clock();
+let previousTime = 0;
 
 const tick = () =>
 {
-    const elapsedTime = clock.getElapsedTime()
+    const elapsedTime = clock.getElapsedTime();
+    const deltaTime = elapsedTime - previousTime;
+    previousTime = elapsedTime;
+
+    // Animate camera
+    camera.position.y = - scrollY / sizes.height * objectsDistance;
+
+    const parallaxX = cursor.x * 0.5;
+    const parallaxY = - cursor.y * 0.5;
+    cameraGroup.position.x += (parallaxX - cameraGroup.position.x) * 5 * deltaTime;
+    cameraGroup.position.y += (parallaxY - cameraGroup.position.y) * 5 * deltaTime;
 
     // Animate meshes
     for (const mesh of sectionMeshes) {
