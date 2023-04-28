3adb5dd7777120d32dcfb485fecdc1e96fd5ea36
Sanambir Singh <Sanambir123@gmail.com>
2023-04-28 13:04:44 +0530

    Add particles to scene

--- 20-scroll-based-animation/src/script.js
+++ 20-scroll-based-animation/src/script.js
@@ -14,6 +14,7 @@ gui
     .addColor(parameters, 'materialColor')
     .onChange(() => {
         material.color.set(parameters.materialColor)
+        particlesMaterial.color.set(parameters.materialColor);
     })
 
 /**
@@ -69,6 +70,33 @@ scene.add(mesh1, mesh2, mesh3);
 
 const sectionMeshes = [ mesh1, mesh2, mesh3 ];
 
+
+/**
+ * Particles
+ */
+// Geometry
+const particlesCount = 200;
+const positions = new Float32Array(particlesCount * 3);
+
+for (let i = 0; i < particlesCount; i++) {
+    positions[i * 3 + 0] = (Math.random() - 0.5) * 10;
+    positions[i * 3 + 1] = objectsDistance * 0.4 - Math.random() * objectsDistance * sectionMeshes.length;
+    positions[i * 3 + 2] = (Math.random() - 0.5) * 10;
+}
+
+const particlesGeometry = new THREE.BufferGeometry();
+particlesGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3));
+
+// Material 
+const particlesMaterial = new THREE.PointsMaterial({
+    color: parameters.materialColor,
+    sizeAttenuation: true,
+    size: 0.03
+})
+
+// Points
+const particles = new THREE.Points(particlesGeometry, particlesMaterial);
+scene.add(particles);
 
 /**
  * Lights
