ae339285b454daaa329ddf4ff0c9212ccffd155a
Sanambir Singh <Sanambir123@gmail.com>
2023-05-08 22:58:31 +0530

    Add physics world using CANNON lib

--- 21-physics/src/script.js
+++ 21-physics/src/script.js
@@ -1,6 +1,7 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'lil-gui'
+import CANNON from 'cannon';
 
 /**
  * Debug
@@ -30,6 +31,32 @@ const environmentMapTexture = cubeTextureLoader.load([
     '/textures/environmentMaps/0/pz.png',
     '/textures/environmentMaps/0/nz.png'
 ])
+
+
+/**
+ * Physics
+ */
+// World
+const world = new CANNON.World();
+world.gravity.set(0, -9.82, 0);
+
+// Sphere
+const sphereShape = new CANNON.Sphere(0.5);
+const sphereBody = new CANNON.Body({
+    mass: 1,
+    position: new CANNON.Vec3(0, 3, 0),
+    shape: sphereShape
+})
+
+world.addBody(sphereBody);
+
+// Floor
+const floorShape = new CANNON.Plane();
+const floorBody = new CANNON.Body();
+floorBody.mass = 0;
+floorBody.addShape(floorShape);
+floorBody.quaternion.setFromAxisAngle(new CANNON.Vec3(-1, 0, 0), Math.PI * 0.5)
+world.addBody(floorBody);
 
 /**
  * Test sphere
@@ -131,10 +158,19 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
  * Animate
  */
 const clock = new THREE.Clock()
+let oldElapsedTime = 0;
 
 const tick = () =>
 {
-    const elapsedTime = clock.getElapsedTime()
+    const elapsedTime = clock.getElapsedTime();
+    const deltaTime = elapsedTime - oldElapsedTime;
+    oldElapsedTime = elapsedTime;
+
+    // Update physics world
+    world.step(1 / 60, oldElapsedTime, 3)
+
+    sphere.position.copy(sphereBody.position)
+
 
     // Update controls
     controls.update()
