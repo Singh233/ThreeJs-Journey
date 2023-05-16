45a118238bac35f23140b0ebb1c79289d40fbf63
Sanambir Singh <Sanambir123@gmail.com>
2023-05-16 22:13:06 +0530

    Add boxes

--- 21-physics/src/script.js
+++ 21-physics/src/script.js
@@ -17,7 +17,20 @@ debugObject.createSphere = () => {
     })
 }
 
+debugObject.createBox = () => {
+  createBox(
+    Math.random(),
+    Math.random(),
+    Math.random(),
+   { 
+      x: (Math.random() - 0.5) * 3, 
+      y: 3, 
+      z: (Math.random() - 0.5) * 3
+  })
+}
+
 gui.add(debugObject, 'createSphere');
+gui.add(debugObject, 'createBox');
 
 /**
  * Base
@@ -161,6 +174,7 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
  */
 const objectsToUpdate = [];
 
+// Sphere
 const SphereGeometry = new THREE.SphereGeometry(1, 20, 20);
 const SphereMaterial = new THREE.MeshStandardMaterial({
     metalness: 0.3,
@@ -198,7 +212,46 @@ const createSphere = (radius, position) => {
   })
 };
 
-createSphere(0.5, { x: 0, y: 3, z: 0 })
+// Box
+const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
+const boxMaterial = new THREE.MeshStandardMaterial({
+    metalness: 0.3,
+    roughness: 0.4,
+    envMap: environmentMapTexture
+})
+
+const createBox = (width, height, depth, position) => {
+  // Three.js mesh
+  const mesh = new THREE.Mesh(
+    boxGeometry,
+    boxMaterial
+  );
+  mesh.scale.set(width, height, depth)
+  mesh.castShadow = true;
+  mesh.position.copy(position);
+  scene.add(mesh)
+
+
+  // Cannon.js body
+  const shape = new CANNON.Box(new CANNON.Vec3(width / 2, height / 2, depth / 2));
+  const body = new CANNON.Body({
+    mass: 1,
+    position: new CANNON.Vec3(0, 3, 0),
+    shape,
+    material: defaultMaterial
+  })
+  body.position.copy(position)
+  world.addBody(body);
+
+  // save in objects to update
+  objectsToUpdate.push({
+    mesh,
+    body,
+  })
+};
+
+
+createBox(1, 1.5, 2, { x: 0, y: 3, z: 0 })
 
 /**
  * Animate
@@ -216,6 +269,7 @@ const tick = () => {
 
   for (const object of objectsToUpdate) {
     object.mesh.position.copy(object.body.position);
+    object.mesh.quaternion.copy(object.body.quaternion);
   }
 
   // Update controls
