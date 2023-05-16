a1561b5dae26e01f5d453f5dd5cca7d670efb3fd
Sanambir Singh <Sanambir123@gmail.com>
2023-05-16 15:47:35 +0530

    Add contact materials and use forces on object

--- 21-physics/src/script.js
+++ 21-physics/src/script.js
@@ -40,14 +40,28 @@ const environmentMapTexture = cubeTextureLoader.load([
 const world = new CANNON.World();
 world.gravity.set(0, -9.82, 0);
 
+// Materials
+const defaultMaterial = new CANNON.Material('default');
+
+const defaultContactMaterial = new CANNON.ContactMaterial(
+    defaultMaterial,
+    defaultMaterial,
+    {
+        friction: 0.1,
+        restitution: 0.4
+    }
+)
+world.addContactMaterial(defaultContactMaterial);
+world.defaultContactMaterial = defaultContactMaterial;
+
 // Sphere
 const sphereShape = new CANNON.Sphere(0.5);
 const sphereBody = new CANNON.Body({
     mass: 1,
     position: new CANNON.Vec3(0, 3, 0),
-    shape: sphereShape
+    shape: sphereShape,
 })
-
+sphereBody.applyLocalForce(new CANNON.Vec3(150, 0, 0), new CANNON.Vec3(0, 0, 0))
 world.addBody(sphereBody);
 
 // Floor
@@ -167,6 +181,8 @@ const tick = () =>
     oldElapsedTime = elapsedTime;
 
     // Update physics world
+    sphereBody.applyForce(new CANNON.Vec3(- 0.5, 0, 0), sphereBody.position)
+
     world.step(1 / 60, oldElapsedTime, 3)
 
     sphere.position.copy(sphereBody.position)
