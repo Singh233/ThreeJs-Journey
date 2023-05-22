c2b89114d195b1a7a05ba26e50aa224e0dcc6e57
Sanambir Singh <Sanambir123@gmail.com>
2023-05-22 22:46:33 +0530

    Use Draco compression and add animation

--- 22-imported-models/src/script.js
+++ 22-imported-models/src/script.js
@@ -1,6 +1,7 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js';
 import * as dat from 'lil-gui';
 
 
@@ -19,15 +20,25 @@ const scene = new THREE.Scene()
 /**
  * Models
  */
-
+const dracoLoader = new DRACOLoader();
+dracoLoader.setDecoderPath('/draco/');
 const gltfLoader = new GLTFLoader();
+gltfLoader.setDRACOLoader(dracoLoader);
+
+let mixer = null;
+
 gltfLoader.load(
-    '/models/FlightHelmet/glTF/FlightHelmet.gltf',
+    '/models/Fox/glTF/Fox.gltf',
     (gltf) => {
         // const children = [...gltf.scene.children];
         // for (const child of children) {
         //     scene.add(child);
         // }
+        mixer = new THREE.AnimationMixer(gltf.scene);
+        const action = mixer.clipAction(gltf.animations[2]);
+        action.play();
+
+        gltf.scene.scale.set(0.025, 0.025, 0.025);
         scene.add(gltf.scene);
     }
 )
@@ -123,7 +134,12 @@ const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
     const deltaTime = elapsedTime - previousTime
-    previousTime = elapsedTime
+    previousTime = elapsedTime;
+
+    // Update mixer
+    if (mixer) {
+        mixer.update(deltaTime);
+    }
 
     // Update controls
     controls.update()
