5a4e2019665c289eac1f42c691bd70a647eec520
Sanambir Singh <Sanambir123@gmail.com>
2023-05-24 15:10:51 +0530

    Use raycaster with mouse events

--- 23-raycaster-and-mouse-events/src/script.js
+++ 23-raycaster-and-mouse-events/src/script.js
@@ -65,6 +65,32 @@ window.addEventListener('resize', () =>
 })
 
 /**
+ * Mouse
+ */
+const mouse = new THREE.Vector2();
+
+window.addEventListener('mousemove', (event) => {
+    mouse.x = event.clientX / sizes.width * 2 - 1;
+    mouse.y = - (event.clientY / sizes.height) * 2 + 1;
+});
+
+window.addEventListener('click', (event) => {
+    if (currentIntersect) {
+        switch (currentIntersect.object) {
+            case object1:
+                console.log('click on object 1');
+                break;
+            case object2:
+                console.log('click on object 2');
+                break;
+            case object3:
+                console.log('click on object 3');
+                break;
+        }
+    }
+});
+
+/**
  * Camera
  */
 // Base camera
@@ -88,7 +114,9 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 /**
  * Animate
  */
-const clock = new THREE.Clock()
+const clock = new THREE.Clock();
+
+let currentIntersect = null;
 
 const tick = () =>
 {
@@ -100,10 +128,12 @@ const tick = () =>
     object3.position.y = Math.sin(elapsedTime * 1.4) * 1.5;
 
     // Cast a ray
-    const rayOrigin = new THREE.Vector3(-3, 0, 0)
-    const rayDirection = new THREE.Vector3(1, 0, 0);
-    rayDirection.normalize();
-    raycaster.set(rayOrigin, rayDirection);
+    raycaster.setFromCamera(mouse, camera);
+
+    // const rayOrigin = new THREE.Vector3(-3, 0, 0)
+    // const rayDirection = new THREE.Vector3(1, 0, 0);
+    // rayDirection.normalize();
+    // raycaster.set(rayOrigin, rayDirection);
 
     const objectsToTest = [object1, object2, object3];
     const intersects = raycaster.intersectObjects(objectsToTest);
@@ -115,6 +145,19 @@ const tick = () =>
     for (const intersect of intersects) {
         intersect.object.material.color.set('#0000ff');
     }
+
+    if (intersects.length) {
+        if (!currentIntersect) {
+            console.log('mouse enter');
+        }
+        currentIntersect = intersects[0];
+    } else {
+        if (currentIntersect) {
+            console.log('mouse leave');
+        }
+        currentIntersect = null;
+    }
+
 
 
     // Update controls
