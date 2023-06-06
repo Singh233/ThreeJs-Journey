4939f53a8731b56d3e09d833c7c715e2b1c9c4e0
Sanambir Singh <Sanambir123@gmail.com>
2023-06-06 15:50:37 +0530

    Add lights, model, and environment map

--- 25-realistic-render/src/script.js
+++ 25-realistic-render/src/script.js
@@ -1,12 +1,22 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'lil-gui'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+
+/**
+ * Loaders
+ */
+const gltfLoader = new GLTFLoader();
+const cubeTextureLoader = new THREE.CubeTextureLoader();
+
+
 
 /**
  * Base
  */
 // Debug
 const gui = new dat.GUI()
+const debugObject = {}
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
@@ -15,13 +25,71 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 /**
- * Test sphere
+ * Update all materials
  */
-const testSphere = new THREE.Mesh(
-    new THREE.SphereGeometry(1, 32, 32),
-    new THREE.MeshBasicMaterial()
+const updateAllMaterials = () => {
+    scene.traverse((child) => {
+        if (child instanceof THREE.Mesh && child.material instanceof THREE.MeshStandardMaterial) {
+            // child.material.envMap = environmentMap;
+            child.material.envMapIntensity = debugObject.envMapIntensity;
+        }
+    })
+}
+
+
+/**
+ * Environment map
+ */
+const environmentMap = cubeTextureLoader.load([
+    '/textures/environmentMaps/0/px.jpg',
+    '/textures/environmentMaps/0/nx.jpg',
+    '/textures/environmentMaps/0/py.jpg',
+    '/textures/environmentMaps/0/ny.jpg',
+    '/textures/environmentMaps/0/pz.jpg',
+    '/textures/environmentMaps/0/nz.jpg',
+])
+
+scene.background = environmentMap;
+scene.environment = environmentMap;
+
+debugObject.envMapIntensity = 5 / 2;
+gui.add(debugObject, 'envMapIntensity').min(0).max(10).step(0.001)
+    .onChange(updateAllMaterials);
+
+
+/**
+ * Models
+ */
+gltfLoader.load(
+    '/models/FlightHelmet/glTF/FlightHelmet.gltf',
+    (gltf) => {
+        gltf.scene.scale.set(10, 10, 10);
+        gltf.scene.position.set(0, -4, 0);
+        gltf.scene.rotation.y = Math.PI * 0.5
+        scene.add(gltf.scene)
+
+        gui.add(gltf.scene.rotation, 'y')
+            .min(- Math.PI)
+            .min(Math.PI)
+            .step(0.001)
+            .name('rotation')
+
+        updateAllMaterials()
+    }
 )
-scene.add(testSphere)
+
+/**
+ * Lights
+ */
+
+const directionalLight = new THREE.DirectionalLight('#ffffff', 3);
+directionalLight.position.set(0.25, 3, -2.25);
+scene.add(directionalLight);
+
+gui.add(directionalLight, 'intensity').min(0).max(10).step(0.001).name('lightintensity');
+gui.add(directionalLight.position, 'x').min(-5).max(5).step(0.001).name('lightX');
+gui.add(directionalLight.position, 'y').min(-5).max(5).step(0.001).name('lightX');
+gui.add(directionalLight.position, 'z').min(-5).max(5).step(0.001).name('lightX');
 
 /**
  * Sizes
@@ -43,7 +111,7 @@ window.addEventListener('resize', () =>
 
     // Update renderer
     renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 })
 
 /**
@@ -66,6 +134,7 @@ const renderer = new THREE.WebGLRenderer({
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+renderer.physicallyCorrectLights = true;
 
 /**
  * Animate
