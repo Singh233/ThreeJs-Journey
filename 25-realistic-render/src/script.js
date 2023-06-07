9de00690003bb2c16ea5a133fda1f1087f73edcb
Sanambir Singh <Sanambir123@gmail.com>
2023-06-07 11:35:29 +0530

    Enable shadows

--- 25-realistic-render/src/script.js
+++ 25-realistic-render/src/script.js
@@ -32,6 +32,9 @@ const updateAllMaterials = () => {
         if (child instanceof THREE.Mesh && child.material instanceof THREE.MeshStandardMaterial) {
             // child.material.envMap = environmentMap;
             child.material.envMapIntensity = debugObject.envMapIntensity;
+            child.material.needsUpdate = true;
+            child.castShadow = true;
+            child.receiveShadow = true
         }
     })
 }
@@ -85,7 +88,14 @@ gltfLoader.load(
 
 const directionalLight = new THREE.DirectionalLight('#ffffff', 3);
 directionalLight.position.set(0.25, 3, -2.25);
+directionalLight.castShadow = true;
+directionalLight.shadow.camera.far = 15;
+directionalLight.shadow.mapSize.set(1024, 1024);
 scene.add(directionalLight);
+
+
+// const directionalLightCameraHelper = new THREE.CameraHelper(directionalLight.shadow.camera);
+// scene.add(directionalLightCameraHelper)
 
 gui.add(directionalLight, 'intensity').min(0).max(10).step(0.001).name('lightintensity');
 gui.add(directionalLight.position, 'x').min(-5).max(5).step(0.001).name('lightX');
@@ -139,7 +149,9 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 renderer.physicallyCorrectLights = true;
 renderer.outputEncoding = THREE.sRGBEncoding;
 renderer.toneMapping = THREE.ReinhardToneMapping;
-renderer.toneMappingExposure = 1
+renderer.toneMappingExposure = 1;
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
 gui.add(renderer, 'toneMapping', {
     No: THREE.NoToneMapping,
