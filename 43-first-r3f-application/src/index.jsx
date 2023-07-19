01b1aa0277e2eebf5d0945252e3e414a64023f87
Sanambir Singh <Sanambir123@gmail.com>
2023-07-19 22:37:03 +0530

    Setup first react three fiber lesson folder

---
+++ 43-first-r3f-application/src/index.jsx
@@ -0,0 +1,10 @@
+import './style.css'
+import ReactDOM from 'react-dom/client'
+
+const root = ReactDOM.createRoot(document.querySelector('#root'))
+
+root.render(
+    <>
+        Soon to be a badass R3F application
+    </>
+)
