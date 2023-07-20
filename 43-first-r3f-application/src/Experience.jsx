32174a45d855847b081b6c2ecb4150553eb382de
Sanambir Singh <Sanambir123@gmail.com>
2023-07-20 12:12:55 +0530

    Group meshes and add animation

--- 43-first-r3f-application/src/Experience.jsx
+++ 43-first-r3f-application/src/Experience.jsx
@@ -1,16 +1,27 @@
-import React from "react";
+import { useFrame } from "@react-three/fiber";
+import React, { useRef } from "react";
 
 export default function Experience() {
+  const cube = useRef();
+  const group = useRef();
+
+  useFrame((state, delta) => {
+    cube.current.rotation.y += delta;
+    group.current.rotation.y += delta;
+  });
   return (
     <>
-      <mesh position-x={-2} rotation-y={Math.PI * 0.32}>
-        <sphereGeometry />
-        <meshBasicMaterial color={"orange"} />
-      </mesh>
-      <mesh scale={1.5} position-x={2} rotation-y={Math.PI * 0.32}>
-        <boxGeometry />
-        <meshBasicMaterial color={"mediumpurple"} />
-      </mesh>
+      <group ref={group}>
+        <mesh position-x={-2} rotation-y={Math.PI * 0.32}>
+          <sphereGeometry />
+          <meshBasicMaterial color={"orange"} />
+        </mesh>
+        <mesh ref={cube} scale={1.5} position-x={2} rotation-y={Math.PI * 0.32}>
+          <boxGeometry />
+          <meshBasicMaterial color={"mediumpurple"} />
+        </mesh>
+      </group>
+
       <mesh scale={10} position-y={-1} rotation-x={-Math.PI * 0.5}>
         <planeGeometry />
         <meshBasicMaterial color={"greenyellow"} />
