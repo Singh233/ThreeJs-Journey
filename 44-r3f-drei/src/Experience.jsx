69e8074bb849fe23c90e2f774b06d81490af9864
Sanambir Singh <Sanambir123@gmail.com>
2023-07-21 22:07:28 +0530

    Add drei TransformControls(gizmo)

--- 44-r3f-drei/src/Experience.jsx
+++ 44-r3f-drei/src/Experience.jsx
@@ -1,9 +1,12 @@
-import { OrbitControls } from "@react-three/drei";
+import { TransformControls, OrbitControls } from "@react-three/drei";
+import { useRef } from "react";
 
 export default function Experience() {
+  const cube = useRef();
+
   return (
     <>
-      <OrbitControls />
+      <OrbitControls makeDefault/>
 
       <directionalLight position={[1, 2, 3]} intensity={1.5} />
       <ambientLight intensity={0.5} />
@@ -13,10 +16,11 @@ export default function Experience() {
         <meshStandardMaterial color="orange" />
       </mesh>
 
-      <mesh position-x={2} scale={1.5}>
+      <mesh ref={cube} position-x={2} scale={1.5}>
         <boxGeometry />
         <meshStandardMaterial color="mediumpurple" />
       </mesh>
+      <TransformControls object={cube} mode="translate" />
 
       <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
         <planeGeometry />
