33b96ebe0bbb1a168f6ff4686b07912f000aba68
Sanambir Singh <Sanambir123@gmail.com>
2023-07-21 11:53:16 +0530

    Add orbit controls and lights

--- 43-first-r3f-application/src/Experience.jsx
+++ 43-first-r3f-application/src/Experience.jsx
@@ -1,30 +1,39 @@
-import { useFrame } from "@react-three/fiber";
+import { extend, useFrame, useThree } from "@react-three/fiber";
 import React, { useRef } from "react";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
+extend({ OrbitControls });
 export default function Experience() {
   const cube = useRef();
   const group = useRef();
+  const { camera, gl } = useThree();
 
   useFrame((state, delta) => {
     cube.current.rotation.y += delta;
-    group.current.rotation.y += delta;
+    // group.current.rotation.y += delta;
   });
+
   return (
     <>
+      <orbitControls args={[camera, gl.domElement]} />
+
+      <ambientLight intensity={0.5} />
+      <directionalLight position={[1, 2, 3]} intensity={1.5} />
+
       <group ref={group}>
         <mesh position-x={-2} rotation-y={Math.PI * 0.32}>
           <sphereGeometry />
-          <meshBasicMaterial color={"orange"} />
+          <meshStandardMaterial color={"orange"} />
         </mesh>
         <mesh ref={cube} scale={1.5} position-x={2} rotation-y={Math.PI * 0.32}>
           <boxGeometry />
-          <meshBasicMaterial color={"mediumpurple"} />
+          <meshStandardMaterial color={"mediumpurple"} />
         </mesh>
       </group>
 
       <mesh scale={10} position-y={-1} rotation-x={-Math.PI * 0.5}>
         <planeGeometry />
-        <meshBasicMaterial color={"greenyellow"} />
+        <meshStandardMaterial color={"greenyellow"} />
       </mesh>
     </>
   );
