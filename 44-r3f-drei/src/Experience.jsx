080acbfe907c8855e827663fd82322b965e3f152
Sanambir Singh <Sanambir123@gmail.com>
2023-07-21 21:43:10 +0530

    Setup r3f and drei lesson folder

---
+++ 44-r3f-drei/src/Experience.jsx
@@ -0,0 +1,33 @@
+import { useThree, extend } from '@react-three/fiber'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+
+extend({ OrbitControls })
+
+export default function Experience()
+{
+    const { camera, gl } = useThree()
+
+    return <>
+
+        <orbitControls args={ [ camera, gl.domElement ] } />
+
+        <directionalLight position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
+        <ambientLight intensity={ 0.5 } />
+
+        <mesh position-x={ - 2 }>
+            <sphereGeometry />
+            <meshStandardMaterial color="orange" />
+        </mesh>
+
+        <mesh position-x={ 2 } scale={ 1.5 }>
+            <boxGeometry />
+            <meshStandardMaterial color="mediumpurple" />
+        </mesh>
+
+        <mesh position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
+            <planeGeometry />
+            <meshStandardMaterial color="greenyellow" />
+        </mesh>
+
+    </>
+}
