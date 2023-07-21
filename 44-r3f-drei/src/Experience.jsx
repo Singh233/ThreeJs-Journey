3f6413582e66e98e5a3dca64db27702e1330b34b
Sanambir Singh <Sanambir123@gmail.com>
2023-07-21 22:18:15 +0530

    Add drei PivotControls to sphere

--- 44-r3f-drei/src/Experience.jsx
+++ 44-r3f-drei/src/Experience.jsx
@@ -1,4 +1,8 @@
-import { TransformControls, OrbitControls } from "@react-three/drei";
+import {
+  PivotControls,
+  TransformControls,
+  OrbitControls,
+} from "@react-three/drei";
 import { useRef } from "react";
 
 export default function Experience() {
@@ -6,15 +10,24 @@ export default function Experience() {
 
   return (
     <>
-      <OrbitControls makeDefault/>
+      <OrbitControls makeDefault />
 
       <directionalLight position={[1, 2, 3]} intensity={1.5} />
       <ambientLight intensity={0.5} />
 
-      <mesh position-x={-2}>
-        <sphereGeometry />
-        <meshStandardMaterial color="orange" />
-      </mesh>
+      <PivotControls
+        anchor={[0, 0, 0]}
+        depthTest={false}
+        lineWidth={4}
+        axisColors={["#9381ff", "#ff4d6d", "#7ae582"]}
+        scale={100}
+        fixed={true}
+      >
+        <mesh position-x={-2}>
+          <sphereGeometry />
+          <meshStandardMaterial color="orange" />
+        </mesh>
+      </PivotControls>
 
       <mesh ref={cube} position-x={2} scale={1.5}>
         <boxGeometry />
