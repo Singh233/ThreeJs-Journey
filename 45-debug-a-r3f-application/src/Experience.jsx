1b931356dbfd20fb6085a67bd284dc58709a720f
Sanambir Singh <Sanambir123@gmail.com>
2023-07-23 16:22:36 +0530

    Monitor performance using r3f-perf

--- 45-debug-a-r3f-application/src/Experience.jsx
+++ 45-debug-a-r3f-application/src/Experience.jsx
@@ -1,27 +1,32 @@
 import { OrbitControls } from "@react-three/drei";
 import { useControls } from "leva";
+import { Perf } from "r3f-perf";
 
 export default function Experience() {
-  const { position, color, visible } = useControls('sphere', {
+  const { perfVisible } = useControls({
+    perfVisible: true,
+  });
+  const { position, color, visible } = useControls("sphere", {
     position: {
       value: { x: -2, y: 0 },
       step: 0.01,
       joystick: "invertY",
     },
-    color: 'orange',
+    color: "orange",
     visible: true,
   });
 
-  const {scale} = useControls('cube', {
+  const { scale } = useControls("cube", {
     scale: {
-        value: 1.5,
-        step: 0.01,
-        min: 0,
-        max: 5
-    }
-  })
+      value: 1.5,
+      step: 0.01,
+      min: 0,
+      max: 5,
+    },
+  });
   return (
     <>
+      {perfVisible && <Perf position="top-left" />}
       <OrbitControls makeDefault />
 
       <directionalLight position={[1, 2, 3]} intensity={1.5} />
