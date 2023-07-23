b289d3f063802d89ef4524c9c9a4c124d96a4d12
Sanambir Singh <Sanambir123@gmail.com>
2023-07-23 16:10:39 +0530

    Add debug ui using leva

--- 45-debug-a-r3f-application/src/Experience.jsx
+++ 45-debug-a-r3f-application/src/Experience.jsx
@@ -1,28 +1,46 @@
-import { OrbitControls } from '@react-three/drei'
+import { OrbitControls } from "@react-three/drei";
+import { useControls } from "leva";
 
-export default function Experience()
-{
-    return <>
+export default function Experience() {
+  const { position, color, visible } = useControls('sphere', {
+    position: {
+      value: { x: -2, y: 0 },
+      step: 0.01,
+      joystick: "invertY",
+    },
+    color: 'orange',
+    visible: true,
+  });
 
-        <OrbitControls makeDefault />
+  const {scale} = useControls('cube', {
+    scale: {
+        value: 1.5,
+        step: 0.01,
+        min: 0,
+        max: 5
+    }
+  })
+  return (
+    <>
+      <OrbitControls makeDefault />
 
-        <directionalLight position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
-        <ambientLight intensity={ 0.5 } />
+      <directionalLight position={[1, 2, 3]} intensity={1.5} />
+      <ambientLight intensity={0.5} />
 
-        <mesh position-x={ - 2 }>
-            <sphereGeometry />
-            <meshStandardMaterial color="orange" />
-        </mesh>
+      <mesh position={[position.x, position.y, 0]} visible={visible}>
+        <sphereGeometry />
+        <meshStandardMaterial color={color} />
+      </mesh>
 
-        <mesh position-x={ 2 } scale={ 1.5 }>
-            <boxGeometry />
-            <meshStandardMaterial color="mediumpurple" />
-        </mesh>
+      <mesh position-x={2} scale={scale}>
+        <boxGeometry />
+        <meshStandardMaterial color="mediumpurple" />
+      </mesh>
 
-        <mesh position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
-            <planeGeometry />
-            <meshStandardMaterial color="greenyellow" />
-        </mesh>
-
+      <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
+        <planeGeometry />
+        <meshStandardMaterial color="greenyellow" />
+      </mesh>
     </>
+  );
 }
--- 45-debug-a-r3f-application/src/index.jsx
+++ 45-debug-a-r3f-application/src/index.jsx
@@ -1,19 +1,24 @@
-import './style.css'
-import ReactDOM from 'react-dom/client'
-import { Canvas } from '@react-three/fiber'
-import Experience from './Experience.jsx'
+import "./style.css";
+import ReactDOM from "react-dom/client";
+import { Canvas } from "@react-three/fiber";
+import Experience from "./Experience.jsx";
+import { StrictMode } from "react";
+import { Leva } from "leva";
 
-const root = ReactDOM.createRoot(document.querySelector('#root'))
+const root = ReactDOM.createRoot(document.querySelector("#root"));
 
 root.render(
+  <StrictMode>
+    <Leva collapsed/>
     <Canvas
-        camera={ {
-            fov: 45,
-            near: 0.1,
-            far: 200,
-            position: [ - 4, 3, 6 ]
-        } }
+      camera={{
+        fov: 45,
+        near: 0.1,
+        far: 200,
+        position: [-4, 3, 6],
+      }}
     >
-        <Experience />
+      <Experience />
     </Canvas>
-)
+  </StrictMode>
+);
