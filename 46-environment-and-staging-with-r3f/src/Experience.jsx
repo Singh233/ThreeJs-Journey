62620337dfa0bf464200c7bc179894d187e52be8
Sanambir Singh <Sanambir123@gmail.com>
2023-07-25 22:59:53 +0530

    Add accumulative shadows(best for still objects)

--- 46-environment-and-staging-with-r3f/src/Experience.jsx
+++ 46-environment-and-staging-with-r3f/src/Experience.jsx
@@ -1,9 +1,11 @@
 import { useFrame } from "@react-three/fiber";
 import {
+  AccumulativeShadows,
   SoftShadows,
   BakeShadows,
   useHelper,
   OrbitControls,
+  RandomizedLight,
 } from "@react-three/drei";
 import { useRef } from "react";
 import { Perf } from "r3f-perf";
@@ -13,28 +15,49 @@ export default function Experience() {
   const cube = useRef();
   const directionalLight = useRef();
 
-  useHelper(directionalLight, THREE.DirectionalLightHelper, 1);
+  // useHelper(directionalLight, THREE.DirectionalLightHelper, 1);
 
   useFrame((state, delta) => {
+    const time = state.clock.elapsedTime;
     cube.current.rotation.y += delta * 0.2;
+    // cube.current.position.x = 2 + Math.sin(time);
   });
 
   return (
     <>
       {/* <BakeShadows /> */}
-      <SoftShadows
+      {/* <SoftShadows
         frustum={3.75}
         size={50}
         near={9.5}
         samples={17}
         rings={11}
-      />
+      /> */}
 
       <color args={["black"]} attach={"background"} />
 
       <Perf position="top-left" />
 
       <OrbitControls makeDefault />
+
+      <AccumulativeShadows
+        position={[0, -0.99, 0]}
+        scale={10}
+        color="#316d39"
+        opacity={0.8}
+        frames={500}
+        temporal
+        // blend={100}
+      >
+        <RandomizedLight
+          amount={8}
+          radius={1}
+          ambient={0.5}
+          intensity={1}
+          position={[1, 2, 3]}
+          bias={0.001}
+        />
+      </AccumulativeShadows>
 
       <directionalLight
         castShadow
@@ -61,12 +84,7 @@ export default function Experience() {
         <meshStandardMaterial color="mediumpurple" />
       </mesh>
 
-      <mesh
-        receiveShadow
-        position-y={-1}
-        rotation-x={-Math.PI * 0.5}
-        scale={10}
-      >
+      <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
         <planeGeometry />
         <meshStandardMaterial color="greenyellow" />
       </mesh>
