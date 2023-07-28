a8ea75f623117b93035edd5f469e193f3ac7a8a6
Sanambir Singh <Sanambir123@gmail.com>
2023-07-28 12:10:02 +0530

    Load models using GLTF and DRACO loader (hard way)

--- 47-load-models-with-r3f/src/Experience.jsx
+++ 47-load-models-with-r3f/src/Experience.jsx
@@ -1,31 +1,36 @@
-import { OrbitControls } from '@react-three/drei'
-import { Perf } from 'r3f-perf'
+import { OrbitControls } from "@react-three/drei";
+import { Perf } from "r3f-perf";
+import { useLoader } from "@react-three/fiber";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader";
 
-export default function Experience()
-{
-    return <>
+export default function Experience() {
+  const model = useLoader(GLTFLoader, "./hamburger-draco.glb", (loader) => {
+    const dracoLoader = new DRACOLoader();
+    dracoLoader.setDecoderPath('./draco/')
+    loader.setDRACOLoader(dracoLoader);
+  });
+  console.log(model);
+  return (
+    <>
+      <Perf position="top-left" />
 
-        <Perf position="top-left" />
+      <OrbitControls makeDefault />
 
-        <OrbitControls makeDefault />
+      <directionalLight castShadow position={[1, 2, 3]} intensity={1.5} />
+      <ambientLight intensity={0.5} />
 
-        <directionalLight castShadow position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
-        <ambientLight intensity={ 0.5 } />
+      <mesh
+        receiveShadow
+        position-y={-1}
+        rotation-x={-Math.PI * 0.5}
+        scale={10}
+      >
+        <planeGeometry />
+        <meshStandardMaterial color="greenyellow" />
+      </mesh>
 
-        <mesh castShadow position-x={ - 2 }>
-            <sphereGeometry />
-            <meshStandardMaterial color="orange" />
-        </mesh>
-
-        <mesh castShadow position-x={ 2 } scale={ 1.5 }>
-            <boxGeometry />
-            <meshStandardMaterial color="mediumpurple" />
-        </mesh>
-
-        <mesh receiveShadow position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
-            <planeGeometry />
-            <meshStandardMaterial color="greenyellow" />
-        </mesh>
-
+      <primitive object={model.scene} scale={0.35} />
     </>
+  );
 }
