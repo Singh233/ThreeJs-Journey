d6126c39b527de80f28a3e19f357bb56e0426ee5
Sanambir Singh <Sanambir123@gmail.com>
2023-07-28 15:47:39 +0530

    use drei gltf loader hook and add multiple model instances

--- 47-load-models-with-r3f/src/Model.jsx
+++ 47-load-models-with-r3f/src/Model.jsx
@@ -1,17 +1,16 @@
 import React from "react";
-import { useLoader } from "@react-three/fiber";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
-import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader";
+import { Clone, useGLTF } from "@react-three/drei";
 
 export default function Model() {
-  const model = useLoader(
-    GLTFLoader,
-    "./hamburger.glb",
-    (loader) => {
-      const dracoLoader = new DRACOLoader();
-      dracoLoader.setDecoderPath("./draco/");
-      loader.setDRACOLoader(dracoLoader);
-    }
+  const model = useGLTF("./hamburger-draco.glb");
+
+  return (
+    <>
+      <Clone object={model.scene} scale={0.35} position-x={-4} />;
+      <Clone object={model.scene} scale={0.35} position-x={0} />;
+      <Clone object={model.scene} scale={0.35} position-x={4} />;
+    </>
   );
-  return <primitive object={model.scene} scale={0.35}/>;
 }
+
+useGLTF.preload("./hamburger-draco.glb");
