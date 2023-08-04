6e8c5759fb364fb01bf87d675c218db706de5a41
Sanambir Singh <Sanambir123@gmail.com>
2023-08-04 12:55:56 +0530

    Add donuts

--- 48-3d-text-with-r3f/src/Experience.jsx
+++ 48-3d-text-with-r3f/src/Experience.jsx
@@ -8,7 +8,6 @@ import { Perf } from "r3f-perf";
 
 export default function Experience() {
   const [matcapTexture] = useMatcapTexture("7B5254_E9DCC7_B19986_C8AC91", 256);
-
   return (
     <>
       <Perf position="top-left" />
@@ -31,6 +30,22 @@ export default function Experience() {
           <meshMatcapMaterial matcap={matcapTexture} />
         </Text3D>
       </Center>
+
+      {[...Array(100)].map((value, index) => (
+        <mesh
+          key={index}
+          position={[
+            (Math.random() - 0.5) * 10,
+            (Math.random() - 0.5) * 10,
+            (Math.random() - 0.5) * 10,
+          ]}
+          scale={0.2 + Math.random() * 0.2}
+          rotation={[Math.random() * Math.PI, Math.random() * Math.PI, 0]}
+        >
+          <torusGeometry args={[1, 0.6, 16, 32]} />
+          <meshMatcapMaterial matcap={matcapTexture} />
+        </mesh>
+      ))}
     </>
   );
 }
