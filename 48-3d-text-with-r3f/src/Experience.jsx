31ad0b1035023253fd1f7770ef18daa189cd81bb
Sanambir Singh <Sanambir123@gmail.com>
2023-08-04 12:23:15 +0530

    Add 3d Text with material using drei helpers

--- 48-3d-text-with-r3f/src/Experience.jsx
+++ 48-3d-text-with-r3f/src/Experience.jsx
@@ -1,18 +1,36 @@
-import { OrbitControls } from '@react-three/drei'
-import { Perf } from 'r3f-perf'
+import {
+  Center,
+  OrbitControls,
+  Text3D,
+  useMatcapTexture,
+} from "@react-three/drei";
+import { Perf } from "r3f-perf";
 
-export default function Experience()
-{
-    return <>
+export default function Experience() {
+  const [matcapTexture] = useMatcapTexture("7B5254_E9DCC7_B19986_C8AC91", 256);
 
-        <Perf position="top-left" />
+  return (
+    <>
+      <Perf position="top-left" />
 
-        <OrbitControls makeDefault />
+      <OrbitControls makeDefault />
 
-        <mesh scale={ 1.5 }>
-            <boxGeometry />
-            <meshNormalMaterial />
-        </mesh>
-
+      <Center>
+        <Text3D
+          font={"./fonts/helvetiker_regular.typeface.json"}
+          size={0.75}
+          height={0.2}
+          curveSegments={12}
+          bevelEnabled
+          bevelThickness={0.02}
+          bevelSize={0.02}
+          bevelOffset={0}
+          bevelSegments={5}
+        >
+          HELLO R3F
+          <meshMatcapMaterial matcap={matcapTexture} />
+        </Text3D>
+      </Center>
     </>
+  );
 }
