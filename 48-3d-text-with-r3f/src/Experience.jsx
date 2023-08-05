ef6cb64fb49aaa040f06994f983552bf96e49580
Sanambir Singh <Sanambir123@gmail.com>
2023-08-05 13:10:29 +0530

    Animate donuts using 2 different techniques

--- 48-3d-text-with-r3f/src/Experience.jsx
+++ 48-3d-text-with-r3f/src/Experience.jsx
@@ -4,8 +4,9 @@ import {
   Text3D,
   useMatcapTexture,
 } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
 import { Perf } from "r3f-perf";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import * as THREE from "three";
 
 const torusGeometry = new THREE.TorusGeometry(1, 0.6, 16, 32);
@@ -15,6 +16,27 @@ export default function Experience() {
   // const [torusGeometry, setTorusGeometry] = useState();
   // const [material, setMaterial] = useState();
   const [matcapTexture] = useMatcapTexture("7B5254_E9DCC7_B19986_C8AC91", 256);
+
+  /**
+   * Animating donuts SOLUTION 1 using group
+   */
+
+  // const donutsGroup = useRef();
+  // useFrame((state, delta) => {
+  //   for (const donut of donutsGroup.current.children) {
+  //     donut.rotation.y += delta * 0.2;
+  //   }
+  // });
+
+  /**
+   * Animating donuts SOLUTION 2 using donuts array
+   */
+  const donuts = useRef([]);
+  useFrame((state, delta) => {
+    for (const donut of donuts.current) {
+      donut.rotation.y += delta * 0.2;
+    }
+  });
 
   useEffect(() => {
     matcapTexture.colorSpace = THREE.SRGBColorSpace;
@@ -49,8 +71,10 @@ export default function Experience() {
         </Text3D>
       </Center>
 
+      {/* <group ref={donutsGroup}> */}
       {[...Array(100)].map((value, index) => (
         <mesh
+          ref={(element) => (donuts.current[index] = element)}
           material={material}
           geometry={torusGeometry}
           key={index}
@@ -63,6 +87,7 @@ export default function Experience() {
           rotation={[Math.random() * Math.PI, Math.random() * Math.PI, 0]}
         />
       ))}
+      {/* </group> */}
     </>
   );
 }
