4472cb1f87dd27cc0d63e212d367aef311d16725
Sanambir Singh <Sanambir123@gmail.com>
2023-08-29 12:35:43 +0530

    Add background and load model using helpers

--- 49-portal-scene-with-r3f/src/Experience.jsx
+++ 49-portal-scene-with-r3f/src/Experience.jsx
@@ -1,15 +1,43 @@
-import { OrbitControls } from '@react-three/drei'
+import { Center, OrbitControls, useGLTF, useTexture } from "@react-three/drei";
+import { DoubleSide } from "three";
 
-export default function Experience()
-{
-    return <>
+export default function Experience() {
+  const { nodes } = useGLTF("./model/portal.glb");
+  const bakedTexture = useTexture("./model/baked.jpg");
+  bakedTexture.flipY = false;
 
-        <OrbitControls makeDefault />
+  return (
+    <>
+      <color args={["#030202"]} attach={"background"} />
+      <OrbitControls makeDefault />
 
-        <mesh scale={ 1.5 }>
-            <boxGeometry />
-            <meshNormalMaterial />
+      <Center>
+        <mesh geometry={nodes.baked.geometry}>
+          <meshBasicMaterial map={bakedTexture} side={DoubleSide} />
         </mesh>
 
+        <mesh
+          geometry={nodes.poleLightA.geometry}
+          position={nodes.poleLightA.position}
+        >
+          <meshBasicMaterial color={"ffffe5"} />
+        </mesh>
+
+        <mesh
+          geometry={nodes.poleLightB.geometry}
+          position={nodes.poleLightB.position}
+        >
+          <meshBasicMaterial color={"ffffe5"} />
+        </mesh>
+
+        <mesh
+          geometry={nodes.portalLight.geometry}
+          position={nodes.portalLight.position}
+          rotation={nodes.portalLight.rotation}
+        >
+          <meshBasicMaterial color={"#ffffff"} />
+        </mesh>
+      </Center>
     </>
+  );
 }
