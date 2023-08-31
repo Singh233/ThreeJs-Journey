f471d2b2fd3227eb44a4bfc7661f0336bd7cd15d
Sanambir Singh <Sanambir123@gmail.com>
2023-08-31 12:07:24 +0530

    Listen to click events on mesh

--- 50-mouse-events-with-r3f/src/Experience.jsx
+++ 50-mouse-events-with-r3f/src/Experience.jsx
@@ -1,37 +1,39 @@
-import { useFrame } from '@react-three/fiber'
-import { OrbitControls } from '@react-three/drei'
-import { useRef } from 'react'
+import { useFrame } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import { useRef } from "react";
 
-export default function Experience()
-{
-    const cube = useRef()
-    
-    useFrame((state, delta) =>
-    {
-        cube.current.rotation.y += delta * 0.2
-    })
+export default function Experience() {
+  const cube = useRef();
 
-    return <>
+  useFrame((state, delta) => {
+    cube.current.rotation.y += delta * 0.2;
+  });
 
-        <OrbitControls makeDefault />
+  const eventHandler = (event) => {
+    cube.current.material.color.set(`hsl(${Math.random() * 360}, 100%, 75%)`);
+  };
 
-        <directionalLight position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
-        <ambientLight intensity={ 0.5 } />
+  return (
+    <>
+      <OrbitControls makeDefault />
 
-        <mesh position-x={ - 2 }>
-            <sphereGeometry />
-            <meshStandardMaterial color="orange" />
-        </mesh>
+      <directionalLight position={[1, 2, 3]} intensity={1.5} />
+      <ambientLight intensity={0.5} />
 
-        <mesh ref={ cube } position-x={ 2 } scale={ 1.5 }>
-            <boxGeometry />
-            <meshStandardMaterial color="mediumpurple" />
-        </mesh>
+      <mesh position-x={-2}>
+        <sphereGeometry />
+        <meshStandardMaterial color="orange" />
+      </mesh>
 
-        <mesh position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
-            <planeGeometry />
-            <meshStandardMaterial color="greenyellow" />
-        </mesh>
+      <mesh ref={cube} position-x={2} scale={1.5} onClick={eventHandler}>
+        <boxGeometry />
+        <meshStandardMaterial color="mediumpurple" />
+      </mesh>
 
+      <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
+        <planeGeometry />
+        <meshStandardMaterial color="greenyellow" />
+      </mesh>
     </>
+  );
 }
