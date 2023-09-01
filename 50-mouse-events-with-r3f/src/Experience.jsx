071f50c73a0409b1e1c2412ab88c4d3a2d5cd0f8
Sanambir Singh <Sanambir123@gmail.com>
2023-09-01 12:39:18 +0530

    Handle click events on complex objects

--- 50-mouse-events-with-r3f/src/Experience.jsx
+++ 50-mouse-events-with-r3f/src/Experience.jsx
@@ -1,9 +1,10 @@
-import { useFrame } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
+import { useFrame, useLoader } from "@react-three/fiber";
+import { OrbitControls, useGLTF } from "@react-three/drei";
 import { useRef } from "react";
 
 export default function Experience() {
   const cube = useRef();
+  const hamburger = useGLTF("hamburger.glb");
 
   useFrame((state, delta) => {
     cube.current.rotation.y += delta * 0.2;
@@ -45,6 +46,16 @@ export default function Experience() {
         <planeGeometry />
         <meshStandardMaterial color="greenyellow" />
       </mesh>
+
+      <primitive
+        object={hamburger.scene}
+        scale={0.25}
+        position-y={0.5}
+        onClick={(event) => {
+          console.log(event.object.name);
+          event.stopPropagation();
+        }}
+      />
     </>
   );
 }
