960fe79ade6b93b1eed81cc970e34510ff69fbf2
Sanambir Singh <Sanambir123@gmail.com>
2023-10-03 11:30:47 +0530

    add noise effect

--- 51-post-processing-with-r3f/src/Experience.jsx
+++ 51-post-processing-with-r3f/src/Experience.jsx
@@ -1,6 +1,11 @@
 import { OrbitControls } from "@react-three/drei";
 import { Perf } from "r3f-perf";
-import { EffectComposer, Glitch, Vignette } from "@react-three/postprocessing";
+import {
+  EffectComposer,
+  Glitch,
+  Noise,
+  Vignette,
+} from "@react-three/postprocessing";
 import { BlendFunction, GlitchMode } from "postprocessing";
 console.log(GlitchMode);
 
@@ -14,12 +19,13 @@ export default function Experience() {
           darkness={0.9}
           blendFunction={BlendFunction.NORMAL}
         /> */}
-        <Glitch
+        {/* <Glitch
           delay={[0.5, 1]}
           duration={[0.1, 0.3]}
           strength={[0.2, 0.4]}
           mode={GlitchMode.SPORADIC}
-        />
+        /> */}
+        <Noise premultiply blendFunction={BlendFunction.SOFT_LIGHT} />
       </EffectComposer>
       <Perf position="top-left" />
 
